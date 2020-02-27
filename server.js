05bc6fda1db4e4103e1b2ed0e6eb34232b656f87
Magno Valdetaro <mvaldetaro@gmail.com>
2020-02-27 19:15:40 -0300

    Teste armazenando a assinatura do cliente

--- server.js
+++ server.js
@@ -17,19 +17,39 @@ const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
 
 webPush.setVapidDetails(config.EMAIL, publicVapidKey, privateVapidKey);
 
+let subscription = {
+    endpoint:
+        'https://fcm.googleapis.com/fcm/send/dCD_f-URa38:APA91bHG_95RZmbBrHeLrWDMTd_NEKHJEp_DH2wkaLqo8z1gmfD9CsDyly_POysq2LFMvElO1PY5RPq7B4C2LoPD4WaL2PmZ7oB7Fn0uC4c68WSW_82JA6lM-2EGLfvqy-m1b_vO3XYM',
+    expirationTime: null,
+    keys: {
+        p256dh:
+            'BBxlFD_Qc752QSDeVQ_8SKpu1VR3h8SDjUWVziu1m1uzL2JAot3GnxgZTgYSaLzzpSExuD5Rr8rHZicNtTd8nz8',
+        auth: 'kCkId27O680WmwrLc3x_rg'
+    }
+};
+
+const payload = JSON.stringify({
+    title: 'Push notifications with Service Workers'
+});
+
 app.post('/subscribe', (req, res) => {
-    const subscription = req.body;
+    const xSubscription = req.body;
+
+    console.log(xSubscription);
 
     res.status(201).json({});
 
-    const payload = JSON.stringify({
-        title: 'Push notifications with Service Workers'
-    });
+    webPush
+        .sendNotification(xSubscription, payload)
+        .catch(rErr => console.error(rErr));
+});
 
+setInterval(() => {
     webPush
         .sendNotification(subscription, payload)
         .catch(rErr => console.error(rErr));
-});
+    console.log('webpush');
+}, 10000);
 
 app.set('port', process.env.PORT || 5000);
 
